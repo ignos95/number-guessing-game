6d2d8a7b4911cb89510d8ce50f683c8560016117
ignos95 <ignos.c.a@gmail.com>
2020-07-02 19:48:45 +0530

    Updates

---
+++ precache-manifest.f1d5d52a13ba9971f7c57a0bee314307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71daaf3c31bf05e835cc761ce789c6f9",
+    "url": "/number-guessing-game/index.html"
+  },
+  {
+    "revision": "21af366396f4761b96a7",
+    "url": "/number-guessing-game/static/css/main.d65969f8.chunk.css"
+  },
+  {
+    "revision": "7b4cdcbd7854027556c6",
+    "url": "/number-guessing-game/static/js/2.5fa6be1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/number-guessing-game/static/js/2.5fa6be1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21af366396f4761b96a7",
+    "url": "/number-guessing-game/static/js/main.4044a106.chunk.js"
+  },
+  {
+    "revision": "c2fd97e322eff67e96e9",
+    "url": "/number-guessing-game/static/js/runtime-main.c2936a1c.js"
+  }
+]);
